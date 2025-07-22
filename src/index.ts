c244157fa89b1754d1ce566f11ad6d68ed54e9cf
Simone <morabitosimone99@gmail.com>
2025-07-22 10:03:02 +0200

    completed ex

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { Todo } from "./types";
-import { User } from "./types";
+import { Todo, User } from "./types";
 
-const todos : Todo[] = [];
+const todos: Todo[] = [];
 
-function addTodo(title: string) : Todo {
+let newId = 1;
+
+function addTodo(title: string): Todo {
     const newTodo: Todo = {
-        id: todos.length + 1,
+        id: newId++, 
         title,
         completed: false
     };
@@ -13,9 +14,53 @@ function addTodo(title: string) : Todo {
     todos.push(newTodo);
     return newTodo
 }
-addTodo("Compra il latte");
-addTodo("Compra i biscotti")
+addTodo("3x8-10 Panca Piana");
+addTodo("3x max Dips")
 
 console.log(todos)
 
+// Associare Todo con Utenti
 
+const users: User[] = [];
+
+let newUserId = 1;
+
+function addUser(name: string, email?: string): User {
+    const newUser: User = {
+        id: newUserId++, // userId :newUserId++
+        name,
+        email
+    };
+
+    users.push(newUser);
+    return newUser
+}
+
+addUser("Giovanni Belardi", "giovanni.belardi@example.com");
+addUser("Franco Neri");
+
+console.log(users);
+
+function assignTodoToUser(todoId: number, userId: number): Todo | string {
+    const todo = todos.find(todo => todo.id === todoId);
+    const user = users.find(user => user.id === userId); // const user = users.find(user => user.userId === userId);
+
+    if (!todo) {
+        return `${todoId} non trovato`;
+    }
+    if (!user) {
+        return `${userId} non trovato`;
+    }
+
+    todo.userId = user.id; // todo.userId = user.userId
+
+    return todo;
+}
+
+assignTodoToUser(1,1);
+assignTodoToUser(2,2);
+
+console.log(todos);
+console.log(users)
+
+// Domanda per Manfro, non sarebbe meglio sostituire id con userId nell'interfaccia User?
